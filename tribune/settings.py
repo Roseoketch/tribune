aeb08b1cafb0b4ef294bca49b828647f9cd3a74a
Roseoketch <oketchrose@gmail.com>
2018-05-16 10:13:38 +0300

    email server changed

--- tribune/settings.py
+++ tribune/settings.py
@@ -32,12 +32,11 @@ ALLOWED_HOSTS = ['*']
 
 
 # Email configurations remember to install python-decouple
-EMAIL_USE_TLS = config('EMAIL_USE_TLS')
-EMAIL_HOST = config('EMAIL_HOST')
-EMAIL_PORT = config('EMAIL_PORT')
-EMAIL_HOST_USER = config('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
-
+EMAIL_USE_TLS=True
+EMAIL_HOST='smtp.gmail.com'
+EMAIL_PORT=587
+EMAIL_HOST_USER='james@moringaschool.com'
+EMAIL_HOST_PASSWORD='James1234'
 
 # Application definition
 
